7aba17376aabdbcec9680926902017936e68a958
Rafael Bermudez Horcajada <rber474@gmail.com>
2024-09-20 13:04:49 +0200

    Back to development: 1.0a9

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = "\n\n".join(
 
 setup(
     name="rber474.releaser",
-    version="1.0a8",
+    version="1.0a9.dev0",
     description="Custom releaser hooks for ONCE project based on zest.releaser",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
